e7a295a76475cb07299a119c13a7229b7f9b2a91
Kalvin Liang Wu <kalvinliang123@gmail.com>
2025-05-27 21:32:14 -0400

    implemented buffer IO to gain speed

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-console.log("Querying api search");
-fetch("/api/search", {
-    method: "POST",
-    headers: {
-        "Content-Type": "text/plain",
-    },
-    body: "glsl function linear interpolation.", // JSON.stringify(`{"query": "bind texture to buffer"}`),
-}).then((response) => console.log(response))
+async function search(prompt) {
+    const result = document.getElementById("results");
+    result.innerHTML = "";
+
+    const response = await fetch("/api/search", {
+        method: "POST",
+        headers: {"Content-Type": "text/plain"},
+        body: prompt,
+    });
+
+    const json = await response.json();
+
+    result.innerHTML = "";
+
+    for ([path, rank] of json) {
+        let item = document.createElement("span");
+        item.appendChild(document.createTextNode(path));
+        item.appendChild(document.createElement("br"));
+        result.appendChild(item);
+    }
+}
+
+let query = document.getElementById("query");
+let currentSearch = Promise.resolve();
+
+query.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
+        currentSearch.then(() => search(query.value));
+    }
+});
