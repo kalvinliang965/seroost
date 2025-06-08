14a07ee631e0a42b76a163315dc7688d5c672813
Kalvin Liang Wu <kalvinliang123@gmail.com>
2025-06-07 20:19:24 -0400

    added document frequency to model

--- src/model.rs
+++ src/model.rs
@@ -1,21 +1,27 @@
 use std::collections::HashMap;
 use std::path::{PathBuf, Path};
-
+use serde::{Deserialize, Serialize};
+pub type DocFreq = HashMap<String, usize>;
 pub type TermFreq = HashMap<String, usize>;
-pub type TermFreqIndex = HashMap<PathBuf, TermFreq>;
+pub type TermFreqPerDoc = HashMap<PathBuf, TermFreq>;
 
+#[derive(Default, Deserialize, Serialize)]
+pub struct Model {
+    pub tfpd: TermFreqPerDoc, // how many time this term appear
+    pub df: DocFreq, // how many document this term exist in 
+}
 
 // apply the term frequency formula
-fn tf(t: &str, d: &TermFreq) -> f32 {
+pub fn compute_tf(t: &str, d: &TermFreq) -> f32 {
     let a = d.get(t).cloned().unwrap_or(0) as f32;
     let b = d.iter().map(|(_, f)| *f).sum::<usize>() as f32;
     a / b
 }
 
-fn idf(t: &str, d: &TermFreqIndex) -> f32 {
-    let N = d.len() as f32;
-    let M = d.values().filter(|tf| tf.contains_key(t)).count() as f32;
-    return (N / M.max(1.0)).log10();
+pub fn compute_idf(t: &str, n:usize, df: &DocFreq) -> f32 {
+    let N = n as f32;
+    let M = df.get(t).cloned().unwrap_or(1) as f32;
+    return (N / M).log10();
 }
 
 
@@ -75,13 +81,13 @@ impl<'a> Iterator for Lexer<'a> {
 }
 
 
-pub fn search_query<'a>(tf_index: &'a TermFreqIndex, query: &'a [char]) -> Vec<(&'a Path, f32)> {
+pub fn search_query<'a>(model: &'a Model, query: &'a [char]) -> Vec<(&'a Path, f32)> {
     let mut result = Vec::<(&Path, f32)>::new();
     let tokens = Lexer::new(&query).collect::<Vec<_>>();
-    for (path, tf_table) in tf_index {
+    for (path, tf_table) in &model.tfpd {
         let mut rank = 0 as f32;
         for token in &tokens {
-            rank += tf(&token, &tf_table) * idf(&token, &tf_index);
+            rank += compute_tf(&token, &tf_table) * compute_idf(&token, model.tfpd.len(), &model.df);
         }
         result.push((path, rank));
     }
